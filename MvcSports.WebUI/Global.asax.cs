a8dccff4c425ddf0970ef48be4530f033cdb998f
knolegsb <seon2you@hotmail.com>
2015-11-21 01:57:30 -0800

    modified Cart Controller

--- MvcSports.WebUI/Global.asax.cs
+++ MvcSports.WebUI/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcSports.Domain.Entities;
+using MvcSports.WebUI.Infrastructure.Binders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +15,11 @@ namespace MvcSports.WebUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
