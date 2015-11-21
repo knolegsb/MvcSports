0c18e084b6dff971b77e173f022704676239f704
knolegsb <seon2you@hotmail.com>
2015-11-21 02:07:52 -0800

    added testing

--- MvcSports.UnitTests/Tests/CartTests.cs
+++ MvcSports.UnitTests/Tests/CartTests.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvcSports.Domain.Entities;
 using System.Linq;
+using MvcSports.Domain.Abstract;
+using Moq;
+using MvcSports.WebUI.Controllers;
 
 namespace MvcSports.UnitTests
 {
@@ -111,6 +114,30 @@ namespace MvcSports.UnitTests
 
             // Assert
             Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+            }.AsQueryable());
+
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            // Act - add a product to the cart
+            target.AddToCart(cart, 1, null);
+
+            // Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
         }
     }
 }
