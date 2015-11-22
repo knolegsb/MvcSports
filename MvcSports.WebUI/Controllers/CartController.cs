9f6b2356653bada52543f754427140625943bdbe
knolegsb <seon2you@hotmail.com>
2015-11-21 22:10:29 -0800

    added shipping details on cart controller

--- MvcSports.WebUI/Controllers/CartController.cs
+++ MvcSports.WebUI/Controllers/CartController.cs
@@ -20,9 +20,10 @@ namespace MvcSports.WebUI.Controllers
         private IProductRepository repository;
         private IOrderProcessor orderProcessor;
 
-        public CartController (IProductRepository repo)
+        public CartController (IProductRepository repo, IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -78,6 +79,26 @@ namespace MvcSports.WebUI.Controllers
         public ViewResult Checkout()
         {
             return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
